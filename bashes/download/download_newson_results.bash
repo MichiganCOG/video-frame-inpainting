da2872999c38c3aab0357cb6e7e0db9100638505
Ryan Szeto <szetor@umich.edu>
2021-08-08 15:32:27 -0400

    Fix box.com references
    Fix #7

--- bashes/download/download_newson_results.bash
+++ bashes/download/download_newson_results.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O newson_results.tar.gz.aa https://umich.box.com/shared/static/35ghqqdwbgav4fhhoehba0bgq7es4ida.aa
-wget -O newson_results.tar.gz.ab https://umich.box.com/shared/static/b4l6gg5invad2yo9mhz6y008nfw2yhkj.ab
+wget -O newson_results.tar.gz.aa https://web.eecs.umich.edu/~szetor/media/bi-TAI-pami/newson_results.tar.gz.aa
+wget -O newson_results.tar.gz.ab https://web.eecs.umich.edu/~szetor/media/bi-TAI-pami/newson_results.tar.gz.ab
 cat newson_results.tar.gz.aa newson_results.tar.gz.ab | tar -xz
 rm newson_results.tar.gz.aa newson_results.tar.gz.ab
